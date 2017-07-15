72ddebac56a6f9502b4fae677f09084ab2fb2037
Nexxkinn <norvin.tambunan@live.com>
2017-07-16 00:27:13 +0700

    Small UI Update : - Switch default font to Montserrat - Change background color

--- Cafeine/Views/ExpandItemDetails.xaml.cs
+++ Cafeine/Views/ExpandItemDetails.xaml.cs
@@ -2,23 +2,9 @@
 using Cafeine.Services;
 using Cafeine.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
-using Microsoft.Toolkit.Uwp.UI.Animations;
 // The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Cafeine
---
+++ Cafeine/Views/View_Resouces/ExpandItemDetailsTemplate.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Cafeine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class ExpandItemDetailsTemplate
+    {
+        public ExpandItemDetailsTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
