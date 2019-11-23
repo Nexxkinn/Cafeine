b6b99721b05e6c2d5481f00fde430ac6e2990701
Nexxkinn <7548170+Nexxkinn@users.noreply.github.com>
2019-11-23 15:57:37 +0700

    Drop custom reveal border light project. Move control namespace to shared control
    [ How the fuck did MS implement that feature only using composition API, though? ]

--- Cafeine.Shared/Controls/CafeineFlyoutButton.cs
+++ Cafeine.Shared/Controls/CafeineFlyoutButton.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.Graphics.Canvas;
-using Microsoft.Graphics.Canvas.Effects;
-using Microsoft.Graphics.Canvas.Geometry;
 using Microsoft.Graphics.Canvas.Text;
 using Microsoft.Graphics.Canvas.UI.Composition;
 using System;
 using System.Diagnostics;
 using System.Numerics;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Graphics.DirectX;
 using Windows.UI;
 using Windows.UI.Composition;
 using Windows.UI.Core;
-using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
@@ -21,18 +17,17 @@ using Windows.UI.Xaml.Media;
 
 // The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
-namespace Cafeine.Views.Resources.Controls
+namespace Cafeine.Shared.Controls
 {
     public class CafeineFlyoutButton : Control
     {
         private Vector2 _size;
-        private TypedEventHandler<CoreWindow,PointerEventArgs> PositionHandler;
 
         // Visual Layer
         private Compositor _compositor;
         private ShapeVisual _shape;
         
-        // DirectX? Layer
+        // Direct2D? Layer
         private CanvasDevice _device;
         private CompositionGraphicsDevice _graphdevice;
         private CompositionDrawingSurface _drawsurface;
@@ -44,7 +39,8 @@ namespace Cafeine.Views.Resources.Controls
         private ColorKeyFrameAnimation _visible;
 
         // border reveal layer
-        private CompositionColorGradientStop lightgradientbrush;
+        private CompositionColorBrush _borderBrushColor;
+        private CompositionSpriteShape _borderBrushShape;
         private ColorKeyFrameAnimation _borderHidden;
         private ColorKeyFrameAnimation _borderVisible;
 
@@ -91,19 +87,20 @@ namespace Cafeine.Views.Resources.Controls
             _graphdevice = CanvasComposition.CreateCompositionGraphicsDevice(_compositor, _device);
             _drawsurface = _graphdevice.CreateDrawingSurface(new Size(Width, Height), DirectXPixelFormat.B8G8R8A8UIntNormalized, DirectXAlphaMode.Premultiplied);
 
+            var dispatcher = ElementCompositionPreview.GetElementVisual(this).Dispatcher;
             DrawShape();
             DrawText();
-            DrawRevealBorder();
-            
+
             ElementCompositionPreview.SetElementChildVisual(this, _shape);
         }
+
         void DrawShape()
         {
             // handle border shape geometry
             using var geo = _compositor.CreateRoundedRectangleGeometry();
             geo.Size = _size;
             geo.CornerRadius = new Vector2(2);
-            _shapecolorbrush = _compositor.CreateColorBrush(Colors.Transparent);
+            _shapecolorbrush = _compositor.CreateColorBrush(Color.FromArgb(30, 0xff, 0xff, 0xff));
             _spriteShape = _compositor.CreateSpriteShape(geo);
             _spriteShape.FillBrush = _shapecolorbrush;
             _shape.Shapes.Add(_spriteShape);
@@ -111,13 +108,15 @@ namespace Cafeine.Views.Resources.Controls
             // Handle Visibility on composition
             _visible = _compositor.CreateColorKeyFrameAnimation();
             _visible.Duration = TimeSpan.FromMilliseconds(100);
-            _visible.InsertKeyFrame(0f, Colors.Transparent);
-            _visible.InsertKeyFrame(1f, Color.FromArgb(30, 0xff, 0xff, 0xff));
+            _visible.InsertKeyFrame(0f, Color.FromArgb(30, 0xff, 0xff, 0xff));
+            _visible.InsertKeyFrame(0.5f, Color.FromArgb(0x40, 0x40, 0x40, 0x40));
+            _visible.InsertKeyFrame(1f, Color.FromArgb(0xcc, 0x26, 0x2a, 0x2f));
 
             _hidden = _compositor.CreateColorKeyFrameAnimation();
             _hidden.Duration = TimeSpan.FromMilliseconds(100);
-            _hidden.InsertKeyFrame(0f, Color.FromArgb(30, 0xff, 0xff, 0xff));
-            _hidden.InsertKeyFrame(1f, Colors.Transparent);
+            _hidden.InsertKeyFrame(0f, Color.FromArgb(0xcc, 0x26, 0x2a, 0x2f));
+            _hidden.InsertKeyFrame(0.5f, Color.FromArgb(0x40, 0x40, 0x40, 0x40));
+            _hidden.InsertKeyFrame(1f, Color.FromArgb(30, 0xff, 0xff, 0xff));
         }
 
         void DrawText()
@@ -130,95 +129,67 @@ namespace Cafeine.Views.Resources.Controls
                 FontSize = (float)FontSize,
                 FontWeight = FontWeight,
                 WordWrapping = CanvasWordWrapping.WholeWord,
+                LineSpacingMode = CanvasLineSpacingMode.Proportional, // no option to make it properly centered.
+                LineSpacing = 0.4f,
+                LineSpacingBaseline = 0.6f,
                 HorizontalAlignment = CanvasHorizontalAlignment.Center,
                 VerticalAlignment = CanvasVerticalAlignment.Center
             };
 
-            // text drawer
-            using (var ds = CanvasComposition.CreateDrawingSession(_drawsurface))
-            {
-                ds.Antialiasing = CanvasAntialiasing.Antialiased;
-                var rectext = new Rect(0, 0, Width, Height);
-                ds.DrawText(Text, rectext, Color.FromArgb(0xd7, 0xff, 0xff, 0xff), textformat);
+            // text drawer method 1 - no anti-aliasing feature
+            //var layout = new CanvasTextLayout(_device, Text, textformat, (float)Width, (float)Height);
+            //var geometry = CanvasGeometry.CreateText(layout);
+            //var compPath = new CompositionPath(geometry);
+            //var pathGeo = _compositor.CreatePathGeometry(compPath);
 
-            }
+            // set to container
+            //var w = _compositor.CreateSpriteShape(pathGeo);
+            //w.FillBrush = _compositor.CreateColorBrush(Color.FromArgb(0xd7, 0xff, 0xff, 0xff));
+            //_shape.Shapes.Add(w);
+
+            // text drawer 2 - with anti-aliasing feature
+            using var ds = CanvasComposition.CreateDrawingSession(_drawsurface);
+            var rectext  = new Rect(0, 0, Width, Height);
+            ds.Antialiasing = CanvasAntialiasing.Antialiased;
+            ds.DrawText(Text, rectext, Color.FromArgb(0xd7, 0xff, 0xff, 0xff), textformat);
+            ds.Flush();
+            ds.Dispose();
 
             // set to container
             var brush = _compositor.CreateSurfaceBrush(_drawsurface);
-            brush.Surface = _drawsurface;
-
             var sprite = _compositor.CreateSpriteVisual();
-            sprite.Brush = brush;
-            sprite.Size = _size;
+                sprite.Brush = brush;
+                sprite.Size = _size;
             _shape.Children.InsertAtTop(sprite);
 
         }
 
-        void DrawRevealBorder()
+        private Vector2 GetElementPosition()
         {
-            // handle reveal brush effect
-            var _revealBorderBrush = _compositor.CreateRadialGradientBrush();
-            CompositionColorGradientStop lightgradientbrush2 = _compositor.CreateColorGradientStop(1, Color.FromArgb(0x00, 0xff, 0xff, 0xff));
-                                         lightgradientbrush  = _compositor.CreateColorGradientStop(0, Color.FromArgb(0x7f, 0xff, 0xff, 0xff));
-            _revealBorderBrush.ColorStops.Add(lightgradientbrush);
-            _revealBorderBrush.ColorStops.Add(lightgradientbrush2);
-            _revealBorderBrush.MappingMode = CompositionMappingMode.Absolute;
-            _revealBorderBrush.EllipseRadius = new Vector2(100f);
-            _revealBorderBrush.Offset = GetPointerPosition(); // reference : https://stackoverflow.com/a/44906442/
-
-            // handle outer shape geometry
-            var bordergeo = _compositor.CreateRoundedRectangleGeometry();
-            bordergeo.Size = Vector2.Subtract(_size, new Vector2(1));
-            bordergeo.Offset = new Vector2(0.5f);
-            bordergeo.CornerRadius = new Vector2(2);
-
-            // create sprite shapes
-            var _borderBrushShape = _compositor.CreateSpriteShape(bordergeo);
-            _borderBrushShape.StrokeBrush = _revealBorderBrush;
-            _borderBrushShape.StrokeThickness = 1;
-            //_fillBrushShape.FillBrush = _compositor.CreateColorBrush(Color.FromArgb(0xff, 0x26, 0x2a, 0x2f));
-
-            // set color animation for _revealBorderBrush
-            _borderVisible = _compositor.CreateColorKeyFrameAnimation();
-            _borderVisible.Duration = TimeSpan.FromMilliseconds(100);
-            _borderVisible.InsertKeyFrame(0f, Color.FromArgb(0x0, 0xff, 0xff, 0xff));
-            _borderVisible.InsertKeyFrame(1f, Color.FromArgb(0x7f, 0xff, 0xff, 0xff));
-
-            _borderHidden = _compositor.CreateColorKeyFrameAnimation();
-            _borderHidden.Duration = TimeSpan.FromMilliseconds(100);
-            _borderHidden.InsertKeyFrame(0f, Color.FromArgb(0x7f, 0xff, 0xff, 0xff));
-            _borderHidden.InsertKeyFrame(1f, Color.FromArgb(0x0, 0xff, 0xff, 0xff));
-
-            // final setup
-            _shape.Shapes.Add(_borderBrushShape);
-            PositionHandler = async (s, e) =>
-            {
-                _revealBorderBrush.Offset = GetPointerPosition();
-            };
-            Window.Current.CoreWindow.PointerMoved += PositionHandler;
+            // current method
+            var Transform = this.TransformToVisual(Window.Current.Content);
+            var ElementPosition = Transform.TransformPoint(new Point(0,0));
+            return ElementPosition.ToVector2();
         }
 
-        private Vector2 GetPointerPosition()
-        {
-            var Transform = this.TransformToVisual(Window.Current.Content);
+        private Vector2 GetPointerPosition() {
+            Vector2 UIPosition = GetElementPosition();
             var pointer = Window.Current.CoreWindow.PointerPosition;
             var bounds = Window.Current.Bounds;
 
-            var point = new Point(pointer.X - bounds.X, pointer.Y - bounds.Y);
-            var position  = Transform.Inverse.TransformPoint(point);
-            return position.ToVector2();
+            var point = new Vector2((float)(pointer.X - bounds.X), (float)(pointer.Y - bounds.Y));
+            var position = point - UIPosition;
+            return position;
         }
 
         protected override void OnPointerEntered(PointerRoutedEventArgs e)
         {
             _shapecolorbrush.StartAnimation("Color", _visible);
-            lightgradientbrush.StartAnimation("Color", _borderHidden);
         }
 
         protected override void OnPointerExited(PointerRoutedEventArgs e)
         {
             _shapecolorbrush.StartAnimation("Color", _hidden);
-            lightgradientbrush.StartAnimation("Color", _borderVisible);
         }
 
         protected override void OnTapped(TappedRoutedEventArgs e)
@@ -231,9 +202,20 @@ namespace Cafeine.Views.Resources.Controls
             base.OnTapped(e);
         }
 
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            _shapecolorbrush.StartAnimation("Color", _visible);
+            base.OnGotFocus(e);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            _shapecolorbrush.StartAnimation("Color", _hidden);
+            base.OnLostFocus(e);
+        }
+
         void OnUnloaded(object sender, RoutedEventArgs e)
         {
-            Window.Current.CoreWindow.PointerMoved -= PositionHandler;
             Unloaded -= OnUnloaded;
             Loading -= OnLoading;
 
